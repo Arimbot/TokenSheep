c3cc3ab4c07161ad525b96d218bd333804efb20a
Arimbot <mail.rim.alex@gmail.com>
2016-02-28 21:29:57 +0100

    Added files via upload

---
+++ Assets/Scripts/game.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game : MonoBehaviour {
+
+	public float objectSpeed = 1.0f;
+	public GameObject treeUp, treeDown, treeLeft;
+	private int score = 0, bestScore = 0;
+	private Vector3 treeUpOrigin, treeDownOrigin, treeLeftOrigin;
+	private int rand = 1;
+	private bool moving = true;
+	static public bool gameStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		if (treeUp)
+			treeUpOrigin = treeUp.transform.position;
+		if (treeDown)
+			treeDownOrigin = treeDown.transform.position;
+		if (treeLeft)
+			treeLeftOrigin = treeLeft.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameStarted) {
+			if (moving) {
+				Vector3 newPosition;
+
+				switch (rand) {
+				case 0:
+					newPosition = treeUp.transform.position;
+					newPosition.x -= objectSpeed * Time.deltaTime;
+
+					treeUp.transform.position = newPosition;
+
+					if (treeUp.transform.position.x <= -35) {
+						treeUp.transform.position = treeUpOrigin;
+						score++;
+						moving = false;
+					}
+					break;
+				case 1:
+					newPosition = treeDown.transform.position;
+					newPosition.x -= objectSpeed * Time.deltaTime;
+
+					treeDown.transform.position = newPosition;
+
+					if (treeDown.transform.position.x <= -35) {
+						treeDown.transform.position = treeDownOrigin;
+						score++;
+						moving = false;
+					}
+					break;
+				case 2:
+					newPosition = treeLeft.transform.position;
+					newPosition.x -= objectSpeed * Time.deltaTime;
+
+					treeLeft.transform.position = newPosition;
+
+					if (treeLeft.transform.position.x <= -35) {
+						treeLeft.transform.position = treeLeftOrigin;
+						score++;
+						moving = false;
+					}
+					break;
+				case 3:
+					newPosition = treeLeft.transform.position;
+					newPosition.x -= objectSpeed * Time.deltaTime;
+					treeLeft.transform.position = newPosition;
+
+					newPosition = treeUp.transform.position;
+					newPosition.x -= objectSpeed * Time.deltaTime;
+					treeUp.transform.position = newPosition;
+
+					if (treeUp.transform.position.x <= -35) {
+						treeUp.transform.position = treeUpOrigin;
+						score++;
+						moving = false;
+					}
+					break;
+				}
+			} else {
+				rand = Random.Range (0, 4);
+				Debug.Log (rand.ToString ());
+				moving = true;
+			}
+		} else {
+			if (Input.GetButtonDown ("Jump")) {
+				gameStarted = true;
+
+				if (score > bestScore)
+					bestScore = score;
+				
+				score = 0;
+				treeUp.transform.position = treeUpOrigin;
+				treeDown.transform.position = treeDownOrigin;
+				treeLeft.transform.position = treeLeftOrigin;
+				moving = true;
+				rand = 1;
+			}
+		}
+	}
+
+	void OnGUI(){
+		Rect position = new Rect (100.0f, 10.0f, 200.0f, 40.0f);
+
+		GUI.Label (position, "Score: " + score.ToString ());
+		position.y = 30.0f;
+		GUI.Label (position, "Highscore: " + bestScore.ToString ());
+
+		if (gameStarted == false) {
+			Rect positionPlay = new Rect (200.0f, 200.0f, 400.0f, 40.0f);
+
+			GUI.Label (positionPlay, "Appuyer sur ESPACE pour commencer");
+		}
+	}
+}
---
+++ Assets/Scripts/jump.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+	public float m_JumpTime = 0.2f;
+	public float m_JumpSpeed = 0.2f;
+	public float m_IncY = 0.5f;
+
+	private float m_JumpValue = 0.0f;
+	private bool m_Jumping = false, m_IsGrounded = true, m_Falling = false;
+	//private Rigidbody m_RigidBody;
+	private Transform m_Transform;
+
+	private Vector3 m_GroundedPosition;
+
+	void Start () {
+		//m_RigidBody = GetComponent<Rigidbody>();
+		m_Transform = GetComponent<Transform>();
+
+		m_GroundedPosition = m_Transform.position;
+	}
+
+
+	void Update () {
+
+	}
+
+
+	private void FixedUpdate(){
+		if (game.gameStarted == true) {
+			if (Input.GetButton ("Jump") && m_IsGrounded && !m_Jumping && !m_Falling) {
+				m_IsGrounded = false;
+				m_Jumping = true;
+			}
+
+			if (m_Jumping) {
+				m_JumpValue += Time.deltaTime * m_JumpSpeed;
+
+				if (m_JumpValue < m_JumpTime) {
+					float ratioAngle = (m_JumpSpeed * Time.deltaTime * 100) / m_JumpTime;
+					m_Transform.transform.Rotate (0f, 0f, (360 / 100) * ratioAngle);
+
+					Debug.Log ("ra= " + ratioAngle);
+
+					Vector3 newPosition = m_Transform.transform.position;
+					newPosition.y += m_IncY;
+
+					m_Transform.transform.position = newPosition;
+				} else {
+					m_Jumping = false;
+					m_Falling = true;
+					m_JumpValue = 0.0f;
+				}
+			}
+
+			if (m_Falling) {
+				m_JumpValue += Time.deltaTime * m_JumpSpeed;
+
+				if (m_JumpValue * 0.5f < m_JumpTime) {
+					float ratioAngle = (m_JumpSpeed * Time.deltaTime * 100) / m_JumpTime;
+					m_Transform.transform.Rotate (0f, 0f, (360 / 100) * ratioAngle);
+
+					Debug.Log ("ra= " + ratioAngle);
+
+					Vector3 newPosition = m_Transform.transform.position;
+					newPosition.y -= m_IncY * 0.5f;
+
+					m_Transform.transform.position = newPosition;
+				} else {
+					m_Jumping = false;
+					m_Falling = false;
+					m_JumpValue = 0.0f;
+					m_Transform.rotation = Quaternion.identity;
+					m_Transform.position = m_GroundedPosition;
+					m_IsGrounded = true;
+				}
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "obstacle") {
+			m_Jumping = false;
+			m_Falling = false;
+			m_JumpValue = 0.0f;
+			m_Transform.rotation = Quaternion.identity;
+			m_Transform.position = m_GroundedPosition;
+			m_IsGrounded = true;
+			game.gameStarted = false;
+		}
+	}
+}
